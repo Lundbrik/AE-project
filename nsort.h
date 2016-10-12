433b264a6ea831ea094ed08ee5ee2d13241122e3
Daniel Lundberg Pedersen <lundbrik@gmail.com>
2016-10-12 23:47:51 +0200

    Added nsort_16().

--- nsort.h
+++ nsort.h
@@ -2,6 +2,130 @@
 
 #define comp_swap(a, b) if (a>b) std::swap(a,b)
 
+// Note that layer numbers are not the actual depth but merely a helper for
+// looking at pictures of the networks.
+
+template <class T>
+void nsort_16(std::vector<T> &data) {
+    // Layer 1
+    comp_swap(data[0], data[1]);
+    comp_swap(data[2], data[3]);
+    comp_swap(data[4], data[5]);
+    comp_swap(data[6], data[7]);
+    comp_swap(data[8], data[9]);
+    comp_swap(data[10], data[11]);
+    comp_swap(data[12], data[13]);
+    comp_swap(data[14], data[15]);
+
+    // Layer 2
+    comp_swap(data[1], data[3]);
+    comp_swap(data[5], data[7]);
+    comp_swap(data[9], data[11]);
+    comp_swap(data[13], data[15]);
+
+    // Layer 3
+    comp_swap(data[0], data[2]);
+    comp_swap(data[4], data[6]);
+    comp_swap(data[8], data[10]);
+    comp_swap(data[12], data[14]);
+
+    // Layer 4
+    comp_swap(data[3], data[7]);
+    comp_swap(data[11], data[15]);
+
+    // Layer 5
+    comp_swap(data[2], data[6]);
+    comp_swap(data[10], data[14]);
+
+    // Layer 6
+    comp_swap(data[1], data[5]);
+    comp_swap(data[9], data[13]);
+
+    // Layer 7
+    comp_swap(data[0], data[4]);
+    comp_swap(data[8], data[12]);
+
+    // Layer 8
+    comp_swap(data[7], data[15]);
+
+    // Layer 9
+    comp_swap(data[6], data[14]);
+
+    // Layer 10
+    comp_swap(data[5], data[13]);
+
+    // Layer 11
+    comp_swap(data[4], data[12]);
+
+    // Layer 12
+    comp_swap(data[3], data[11]);
+
+    // Layer 13
+    comp_swap(data[2], data[10]);
+
+    // Layer 14
+    comp_swap(data[1], data[9]);
+
+    // Layer 15
+    comp_swap(data[0], data[8]);
+
+    // Layer 16
+    comp_swap(data[5], data[10]);
+
+    // Layer 17
+    comp_swap(data[6], data[9]);
+
+    // Layer 18
+    comp_swap(data[1], data[2]);
+    comp_swap(data[3], data[12]);
+
+    // Layer 19
+    comp_swap(data[4], data[8]);
+
+    // Layer 20
+    comp_swap(data[7], data[11]);
+    comp_swap(data[13], data[14]);
+
+    // Layer 21
+    comp_swap(data[2], data[8]);
+    comp_swap(data[11], data[14]);
+
+    // Layer 22
+    comp_swap(data[1], data[4]);
+    comp_swap(data[7], data[13]);
+
+    // Layer 23
+    comp_swap(data[2], data[4]);
+    comp_swap(data[5], data[6]);
+    comp_swap(data[9], data[10]);
+    comp_swap(data[11], data[13]);
+
+    // Layer 24
+    comp_swap(data[7], data[12]);
+
+    // Layer 25
+    comp_swap(data[3], data[8]);
+
+    // Layer 26
+    comp_swap(data[3], data[5]);
+    comp_swap(data[7], data[9]);
+
+    // Layer 27
+    comp_swap(data[6], data[8]);
+    comp_swap(data[10], data[12]);
+
+    // Layer 28
+    comp_swap(data[3], data[4]);
+    comp_swap(data[5], data[6]);
+    comp_swap(data[7], data[8]);
+    comp_swap(data[9], data[10]);
+    comp_swap(data[11], data[12]);
+
+    // Layer 29
+    comp_swap(data[6], data[7]);
+    comp_swap(data[8], data[9]);
+}
+
 template <class T>
 void nsort_12(std::vector<T> &data) {
     // Layer 1
